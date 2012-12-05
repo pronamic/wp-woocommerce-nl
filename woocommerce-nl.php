d970a5d927dd25dc3f950422c2a4cc43041f20f5
Remco Tolsma <info@remcotolsma.nl>
2012-12-05 10:46:59 +0100

    Updated version to 0.3.19.

--- woocommerce-nl.php
+++ woocommerce-nl.php
@@ -4,7 +4,7 @@ Plugin Name: WooCommerce (nl)
 Plugin URI: http://pronamic.eu/wp-plugins/woocommerce-nl/
 Description: Extends the WooCommerce plugin and add-ons with the Dutch language: <strong>WooCommerce</strong> 1.6.5.2 | <strong>WooCommerce EU VAT Number</strong> 1.4 | <strong>WooCommerce Subscribe to Newsletter</strong> 1.0.4 
 
-Version: 0.3.18
+Version: 0.3.19
 Requires at least: 3.0
 
 Author: Pronamic
