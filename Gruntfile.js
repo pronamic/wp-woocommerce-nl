f4f055f83d6dea195615bef769ad46442534372b
Remco Tolsma <info@remcotolsma.nl>
2014-03-05 10:54:55 +0100

    Use Grunt to download PO files from GlotPress and generate the MO files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,9 @@ module.exports = function( grunt ) {
 		// Package
 		pkg: grunt.file.readJSON( 'package.json' ),
 		
+		// WooCommerce
+		wooCommerceVersion: '2.1.3',
+		
 		// PHPLint
 		phplint: {
 			options: {
@@ -41,13 +44,41 @@ module.exports = function( grunt ) {
 					type: 'wp-plugin'
 				}
 			}
-		}
+		},
+		
+		// Shell
+		shell: {
+			downloadPo: {
+				command: [
+					'GLOTPRESS_URL=http://glotpress.pronamic.nl/projects/woocommerce/<%= wooCommerceVersion %>/nl/nl_NL/export-translations',
+					'PO_FILE=languages/woocommerce/nl_NL.po',
+					'touch $PO_FILE',
+					'wget -O $PO_FILE $GLOTPRESS_URL'
+				].join( '&&' )
+	        },
+			downloadAdminPo: {
+				command: [
+					'GLOTPRESS_URL=http://glotpress.pronamic.nl/projects/woocommerce/<%= wooCommerceVersion %>/admin/nl/nl_NL/export-translations',
+					'PO_FILE=languages/woocommerce/admin-nl_NL.po',
+					'touch $PO_FILE',
+					'wget -O $PO_FILE $GLOTPRESS_URL'
+				].join( '&&' )
+	        },
+	    	generateMos: {
+		    	command: [
+		  		    'cd languages',
+			    	'for i in **/*.po; do msgfmt $i -o ${i%%.*}.mo; done'
+			    ].join( '&&' )
+		    },
+	    }
 	} );
 
 	grunt.loadNpmTasks( 'grunt-phplint' );
 	grunt.loadNpmTasks( 'grunt-checkwpversion' );
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-shell' );
 
 	// Default task(s).
-	grunt.registerTask( 'default', [ 'phplint', 'checkwpversion', 'makepot' ] );
+	grunt.registerTask( 'default', [ 'phplint', 'checkwpversion', 'makepot', 'shell:downloadPo' ] );
+	grunt.registerTask( 'downloadPo', [ 'shell:downloadPo', 'shell:downloadAdminPo', 'shell:generateMos' ] );
 };
